c825cdaca50f83ec30bd58b96263dabc8991bc12
yuanman001 <yuanman@asiainfo.com>
2017-01-06 17:24:14 +0800

    还原

---
+++ iPaaS-Common/src/main/java/com/ai/paas/ipaas/common/service/impl/OrgnizeUserHelper.java
@@ -0,0 +1,29 @@
+package com.ai.paas.ipaas.common.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ai.paas.ipaas.PaasException;
+import com.ai.paas.ipaas.ServiceUtil;
+import com.ai.paas.ipaas.common.dao.interfaces.OrgnizeUserInfoMapper;
+import com.ai.paas.ipaas.common.dao.mapper.bo.OrgnizeUserInfo;
+import com.ai.paas.ipaas.common.dao.mapper.bo.OrgnizeUserInfoCriteria;
+import com.ai.paas.ipaas.common.service.IOrgnizeUserHelper;
+
+@Service
+@Transactional
+public class OrgnizeUserHelper implements IOrgnizeUserHelper{
+	public OrgnizeUserInfo getOrgnizeInfo(String userId) throws PaasException {
+		OrgnizeUserInfoMapper mapper = ServiceUtil.getMapper(OrgnizeUserInfoMapper.class);
+		OrgnizeUserInfoCriteria condition = new OrgnizeUserInfoCriteria();
+		condition.createCriteria().andUserIdEqualTo(userId);
+		List<OrgnizeUserInfo> beans = mapper.selectByExample(condition);
+		if(beans.size()!=1){ 
+			new PaasException("get orgnize info error!");
+		}
+		
+		return beans.get(0);
+	}
+}

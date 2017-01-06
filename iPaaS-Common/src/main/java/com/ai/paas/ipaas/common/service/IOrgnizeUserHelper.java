490da88a468c256570a149a1032aa5ef65c31d62
yuanman001 <yuanman@asiainfo.com>
2017-01-06 17:09:19 +0800

    删除重复的类

--- iPaaS-Common/src/main/java/com/ai/paas/ipaas/common/service/IOrgnizeUserHelper.java
+++
@@ -1,8 +0,0 @@
-package com.ai.paas.ipaas.common.service;
-
-import com.ai.paas.ipaas.PaasException;
-import com.ai.paas.ipaas.common.dao.mapper.bo.OrgnizeUserInfo;
-
-public interface IOrgnizeUserHelper {
-	OrgnizeUserInfo getOrgnizeInfo(String userId) throws PaasException;
-}
